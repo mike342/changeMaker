281dab1d7210d88835a4141cab87bd3f34faa342
SeedPaths-User <lessdismissive@email.com>
2014-04-17 13:37:02 -0600

    take in one of these Dice notation  commands and correctly generates the appropriate random numbers.

---
+++ changeMaker/changeMaker/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changeMaker
+{
+    class Program
+    {
+        static void changeMaker(double amount)
+        {
+            // printing the amount or (input) first
+            Console.WriteLine(amount);
+            // declering the variables
+            var quarters = 0;
+            var dimes = 0;
+            var nickles = 0;
+            var pennies = 0;
+            //doing our while loop for the quarter
+
+            while (amount > .25)
+            {
+                quarters++;
+                amount -= .25;
+            }//while loop for the dime
+            while (amount > .10)
+            {
+                dimes++;
+                amount -= .10;
+            }//while loop for the nickles
+            while (amount > .05)
+            {
+                nickles++;
+                amount -= .05;
+            }//while loop for the pennie
+            while (amount > .01)
+            {
+                pennies++;
+                amount -= .01;
+
+            }//telling the comp. to print out our quarter,dime,nickles and penny value each on a new line
+            Console.WriteLine(quarters);
+            Console.WriteLine(dimes);
+            Console.WriteLine(nickles);
+            Console.WriteLine(pennies);
+
+
+        }
+        static void Main(string[] args)
+        {
+            changeMaker(3.18);
+            changeMaker(0.99);
+            changeMaker(12.93);
+            Console.ReadKey();
+
+        }
+    }
+}
